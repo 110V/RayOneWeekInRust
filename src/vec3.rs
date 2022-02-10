dc26b1ad8687d82843501f5709b9d68663966882
110v <sgb1616@gmail.com>
2022-02-10 23:01:19 +0900

    feat : add ops

--- src/vec3.rs
+++ src/vec3.rs
@@ -1,5 +1,7 @@
-use std::ops::Add;
-use num::{Float,Integer};
+use num::{ToPrimitive};
+use std::ops::{Add, Div, Mul, Sub};
+use std::borrow::{Cow,Borrow};
+use std::convert::AsRef;
 #[derive(Copy, Clone, Debug, PartialEq, PartialOrd)]
 pub struct Vec3 {
     pub x: f32,
@@ -12,7 +14,6 @@ impl Vec3 {
         Vec3 { x, y, z }
     }
 }
-
 
 macro_rules! impl_binary_operations {
     ($VectorType:ident $Operation:ident $op_fn:ident $op_symbol:tt) => {
@@ -51,21 +52,43 @@ macro_rules! impl_binary_operations {
 
     }
 }
+
+macro_rules! impl_number_operations {
+  ($VectorType:ident $Operation:ident $op_fn:ident $op_symbol:tt) => {
+
+    impl<'a,'b,T:ToPrimitive> $Operation<T> for &'b $VectorType {
+      type Output = $VectorType;
+      fn $op_fn(self, other:T) -> $VectorType {
+        $VectorType{
+          x: self.x $op_symbol other.to_f32().unwrap(),
+          y: self.y $op_symbol other.to_f32().unwrap(),
+          z: self.z $op_symbol other.to_f32().unwrap()
+        }
+      }
+    }
+    impl<T:ToPrimitive> $Operation<T> for $VectorType {
+      type Output = $VectorType;
+      fn $op_fn(self, other:T) -> $VectorType {
+        &self $op_symbol other
+      }
+    }
+  }
+}
+
+
 impl_binary_operations!(Vec3 Add add +);
+impl_number_operations!(Vec3 Add add +);
+
+impl_binary_operations!(Vec3 Sub sub -);
+impl_number_operations!(Vec3 Sub sub -);
+
+impl_binary_operations!(Vec3 Mul mul *);
+impl_number_operations!(Vec3 Mul mul *);
+
+impl_binary_operations!(Vec3 Div div /);
+impl_number_operations!(Vec3 Div div /);
 
 
-// impl<'a,T:Float+Integer> $Operation<T> for &'a $VectorType {
-//   type Output = $VectorType;
-
-//   fn $op_fn(self, other: T) -> $VectorType {
-//     $VectorType {
-//       x: self.x $op_symbol other,
-//       y: self.y $op_symbol other,
-//       z: self.z $op_symbol other
-//     }
-//   }
-
-// }
 
 #[cfg(test)]
 mod tests {
@@ -75,9 +98,25 @@ mod tests {
     fn add() {
         let a = Vec3::new(0.0, 1.0, 2.0);
         let b = Vec3::new(3.0, 4.0, 5.0);
+        let c:u32 = 3;
+        let d:i32 = -3;
+        let e:f32 = 3.0;
+        let f:usize = 10;
         assert_eq!(&a + &b, Vec3::new(3.0, 5.0, 7.0));
         assert_eq!(a + &b, Vec3::new(3.0, 5.0, 7.0));
         assert_eq!(&a + b, Vec3::new(3.0, 5.0, 7.0));
         assert_eq!(a + b, Vec3::new(3.0, 5.0, 7.0));
+        println!("{:?},{:?},{:?},{:?},{:?},{:?}",a,b,c,d,e,f);
+
+        assert_eq!(&a+c, Vec3::new(3.0, 4.0, 5.0));
+        assert_eq!(&a+c, Vec3::new(3.0, 4.0, 5.0));
+        assert_eq!(a+c, Vec3::new(3.0, 4.0, 5.0));
+        assert_eq!(&a+d, Vec3::new(-3.0, -2.0, -1.0));
+        assert_eq!(&a+e, Vec3::new(3.0, 4.0, 5.0));
+        assert_eq!(&a+f, Vec3::new(10.0, 11.0, 12.0));
     }
-  }
+
+    fn num_test(){
+      
+    }
+}
